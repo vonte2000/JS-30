2ebab5a2fc8f69aeb52d43b4a3b4b24fc0ad51d0
DeVonte Gooden <dg208804@gmail.com>
2025-04-30 03:24:17 -0400

    completed sorting without articles

--- 17:Sort/index.js
+++ 17:Sort/index.js
@@ -13,3 +13,15 @@ const bands = [
     'Anywhere But Here', 
     'An Old Dog'
 ];
+
+function strip(bandName) {
+    return bandName.replace(/^(a |the |an )/i, '').trim() //removes the articles from named lists
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1: -1)
+
+document.querySelector('#bands').innerHTML = 
+    sortedBands
+        .map(band => `<li>${band}</li>`)
+        .join('')
+console.log(sortedBands)
